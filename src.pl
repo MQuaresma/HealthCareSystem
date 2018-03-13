e8ff54e8505e76f04e9678c17014b8bfb3e6b507
jcm300 <zeclmartins@gmail.com>
2018-03-13 16:59:31 +0000

    guide file

---
+++ src.pl
@@ -0,0 +1,23 @@
+# utente: #IdUt, Nome, Idade, Morada -> {V,F}
+
+# prestador: #IdPrest, Nome, Especialidade, Instituição -> {V,F}
+
+# cuidado: Data, #IdUt, #IdPrest, Descrição, Custo -> {V,F}
+
+# Registar utentes, prestadores e cuidados de saúde
+
+# Remover utentes, prestadores e cuidados de saúde
+
+# Identificar utentes por critérios de seleção
+
+# Identificar as instituições prestadoras de cuidados de saúde
+
+# Identificar cuidados de saúde prestados por instituição/cidade/datas
+
+# Identificar os utentes de um prestador/especialidade/instituição
+
+# Identificar cuidados de saúde realizados por utente/instituição/prestador
+
+# Determinar todas as instituições/prestadores a que um utente já recorreu
+
+# Calcular o custo total dos cuidados de saúde por utente/especialidade/prestador/datas
