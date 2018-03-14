131b3fc281325d7b78737a63089ba6f416025af8
jcm300 <zeclmartins@gmail.com>
2018-03-14 23:05:25 +0000

    adding knowledge

--- src.pl
+++ src.pl
@@ -28,11 +28,19 @@ utente(1,joao,23,porto).
 utente(2,maria,45,braga).
 utente(3,joao,45,setubal).
 utente(4,joana,25,braga).
+utente(5,miguel,33,evora).
+utente(6,carla,60,coimbra).
+utente(7,marta,56,braga).
+utente(8,guilherme,23,santarem).
+utente(9,tatiana,76,faro).
+utente(10,beatriz,28,chaves).
 
 % prestador: #IdPrest, Nome, Especialidade, Instituição -> {V,F}
 prestador(1,jose,pediatria,hospital_Braga).
 prestador(2,simao,oftalmologia,hospital_Lisboa).
 prestador(3,alfredo,medicina_Geral,hospital_Guimaraes).
+prestador(4,margarida,enfermagem,hospital_Coimbra).
+prestador(5,antonio,medicina_Geral,hospital_Coimbra).
 
 % cuidado: Data, #IdUt, #IdPrest, Descrição, Custo -> {V,F}
 cuidado(2016-05-23,1,1,consulta,20).
@@ -44,59 +52,59 @@ cuidado(2017-05-20,1,3,consulta,15).
 
 solucoes(T,Q,S):-findall(T,Q,S).
 
-% Registar utentes, prestadores e cuidados de saúde
+%Registar utentes, prestadores e cuidados de saúde
 registarUtente(Id,Nome,Idade,Morada):-evolucao(utente(Id,Nome,Idade,Morada)).
 
 registarPrestador(Id,Nome,Esp,Inst):-evolucao(prestador(Id,Nome,Esp,Inst)).
 
 registarCuidado(Data,IdU,IdPrest,Desc,Custo):-evolucao(cuidado(Data,IdU,IdPrest,Desc,Custo)).
 
-% Remover utentes, prestadores e cuidados de saúde
+%Remover utentes, prestadores e cuidados de saúde
 removerUtente(Id,Nome,Idade,Morada):-involucao(utente(Id,Nome,Idade,Morada)).
 
 removerPrestador(Id,Nome,Esp,Inst):-involucao(prestador(Id,Nome,Esp,Inst)).
 
 removerCuidado(Data,IdU,IdPrest,Desc,Custo):-involucao(cuidado(Data,IdU,IdPrest,Desc,Custo)).
 
-% Identificar utentes por critérios de seleção
-% identificaUtente : nome, NomeUtente, Solução -> {V,F}
+%Identificar utentes por critérios de seleção
+%identificaUtente : nome, NomeUtente, Solução -> {V,F}
 identificaUtente(nome,Nome,S) :-
 		solucoes(utente(X,Nome,Y,Z),utente(X,Nome,Y,Z),S).
-% identificaUtente : idade, IdadeUtente, Solução -> {V,F}		
+%identificaUtente : idade, IdadeUtente, Solução -> {V,F}		
 identificaUtente(idade,Idade,S) :-
 		solucoes(utente(X,Y,Idade,Z),utente(X,Y,Idade,Z),S).
-% identificaUtente : morada, MoradaUtente, Solução -> {V,F}		
+%identificaUtente : morada, MoradaUtente, Solução -> {V,F}		
 identificaUtente(morada,Morada,S) :-
 		solucoes(utente(X,Y,Z,Morada),utente(X,Y,Z,Morada),S).
 
-% Identificar as instituições prestadoras de cuidados de saúde
-% identificaInstituicoes : Solução -> {V,F}
+%Identificar as instituições prestadoras de cuidados de saúde
+%identificaInstituicoes : Solução -> {V,F}
 identificaInstituicoes(S) :-
 		solucoes((I),(prestador(X,Y,Z,I)),S).
 
-% Identificar cuidados de saúde prestados por instituição/cidade/datas
-% identCuiPrest : instituicao, Instituicao, Resultado -> {V,F}
+%Identificar cuidados de saúde prestados por instituição/cidade/datas
+%identCuiPrest : instituicao, Instituicao, Resultado -> {V,F}
 identCuidPrest(instituicao,Ins,R):- 
         solucoes( Esp, prestador(IdP,No,Esp,Ins),R).
-% identCuiPrest : cidade, Cidade, Resultado -> {V,F}
+%identCuiPrest : cidade, Cidade, Resultado -> {V,F}
 identCuidPrest(cidade,Cid,R):-
         solucoes( Esp, (cuidado(Da,IdU,IdP,Desc,C), utente(IdU,NoU,Id,Cid), prestador(IdP,NoP,Esp,Ins)),R).
-% identCuiPrest : datas, Data, Data, Resultado -> {V,F}
+%identCuiPrest : datas, Data, Data, Resultado -> {V,F}
 identCuidPrest(datas,Data1,Data2,R):-
         solucoes( Esp, (prestador(IdP,No,Esp,Ins), cuidado(Da,IdU,IdP,Desc,C), Da @< Data2, Data1 @< Da),R).
 
-% Identificar os utentes de um prestador/especialidade/instituição
-% identUtentes : prestador, Nome, Resultado -> {V,F}
+%Identificar os utentes de um prestador/especialidade/instituição
+%identUtentes : prestador, Nome, Resultado -> {V,F}
 identUtentes(prestador,PrestadorNome,R):-
         solucoes( NoU, (prestador(IdP,PrestadorNome,Esp,Ins), cuidado(Da,IdU,IdP,Desc,C), utente(IdU,NoU,Id,Cid)),R).
-% identUtentes : especialidade, Especialidade, Resultado -> {V,F}
+%identUtentes : especialidade, Especialidade, Resultado -> {V,F}
 identUtentes(especialidade,Espec,R):-
         solucoes( NoU, (prestador(IdP,NoP,Espec,Ins), cuidado(Da,IdU,IdP,Desc,C), utente(IdU,NoU,Id,Cid)),R).
-% identUtentes : instituicao, Instituicao, Resultado -> {V,F}
+%identUtentes : instituicao, Instituicao, Resultado -> {V,F}
 identUtentes(instituicao,Ins,R):-
         solucoes( NoU, (prestador(IdP,NoP,Esp,Ins), cuidado(Da,IdU,IdP,Desc,C), utente(IdU,NoU,Id,Cid)),R).
 
-% Identificar cuidados de saúde realizados por utente/instituição/prestador
+%Identificar cuidados de saúde realizados por utente/instituição/prestador
 identificaCuidadosRealizados(utente,IdU,R):-
         solucoes( cuidado(Da,IdU,IdP,Desc,C), cuidado(Da,IdU,IdP,Desc,C),R).
 
@@ -106,12 +114,13 @@ identificaCuidadosRealizados(instituicao,Ins,R):-
 identificaCuidadosRealizados(prestador,IdP,R):-
         solucoes( cuidado(Da,IdU,IdP,Desc,C), cuidado(Da,IdU,IdP,Desc,C),R).
 
-% Determinar todas as instituições/prestadores a que um utente já recorreu
+%Determinar todas as instituições/prestadores a que um utente já recorreu
 porUtente(instituicao,IdU,R):-
 		solucoes( Ins, (cuidado(Da,IdU,IdP,Desc,C), prestador(IdP,No,Esp,Ins)),R).
 porUtente(prestador,IdU,R):-
 		solucoes( (IdP,No), (cuidado(Da,IdU,IdP,Desc,C), prestador(IdP,No,Esp,Ins)),R).
-% Calcular o custo total dos cuidados de saúde por utente/especialidade/prestador/datas
+
+%Calcular o custo total dos cuidados de saúde por utente/especialidade/prestador/datas
 
 %funçao auxiliar para somar uma lista
 somaLista([],0).
